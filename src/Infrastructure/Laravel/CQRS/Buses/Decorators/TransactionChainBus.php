4ad0550d30e31ce8984e63faad3c9a32b16c2cd1
daaeev <LfdblGbhju@gmail.com>
2023-03-10 18:34:25 +0200

    Added DB transaction chain bus

---
+++ src/Infrastructure/Laravel/CQRS/Buses/Decorators/TransactionChainBus.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Project\Infrastructure\Laravel\CQRS\Buses\Decorators;
+
+use Illuminate\Support\Facades\DB;
+use Project\Common\CQRS\Buses\Interfaces;
+use Project\Common\CQRS\Buses\Interfaces\RequestBus;
+
+class TransactionChainBus implements Interfaces\ChainBus
+{
+    private Interfaces\ChainBus $decorated;
+
+    public function __construct(Interfaces\ChainBus $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function dispatch($command): mixed
+    {
+        DB::beginTransaction();
+
+        try {
+            $result = $this->decorated->dispatch($command);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+
+        return $result;
+    }
+
+    public function registerBus(RequestBus $bus): void
+    {
+        $this->decorated->registerBus($bus);
+    }
+}
