6c9f4e24601132b1bdd1726ec0dcaea4c8825837
daaeev <LfdblGbhju@gmail.com>
2023-06-02 17:34:26 +0300

    Added custom exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,47 +2,87 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\App;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
+
+    public function render($request, Throwable $e)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($request->is('api/*') || $request->wantsJson()) {
+            $response = $this->renderExceptionAsJson($this->prepareException($e));
+        } else {
+            $response = parent::render($request, $e);
+        }
+
+        return $response;
+    }
+
+    protected function prepareException(Throwable $e)
+    {
+        return match (true) {
+            $e instanceof \DomainException => new HttpException(422, $e->getMessage(), $e),
+            $e instanceof AuthenticationException => new HttpException(401, $e->getMessage(), $e),
+            default => parent::prepareException($e),
+        };
+    }
+
+    private function renderExceptionAsJson(Throwable $e): JsonResponse
+    {
+        return match (true) {
+            $e instanceof ValidationException => $this->renderInvalidatedAsJson($e),
+            $e instanceof HttpExceptionInterface => $this->renderHttpExceptionAsJson($e),
+            default => $this->renderFallback($e),
+        };
+    }
+
+    protected function renderInvalidatedAsJson(ValidationException $e): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Error',
+            'code' => 422,
+            'message' => 'Validation failed',
+            'errors' => $e->errors(),
+        ], 422);
+    }
+
+    protected function renderHttpExceptionAsJson(HttpExceptionInterface $e): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Error',
+            'code' => $e->getStatusCode() ?: 500,
+            'message' => $e->getMessage(),
+        ], $e->getStatusCode() ?: 500);
+    }
+
+    protected function renderFallback(Throwable $e): JsonResponse
+    {
+        $data = [
+            'status' => 'Error',
+            'code' => 500,
+            'message' => 'Something went wrong',
+        ];
+
+        if (!App::isProduction() && config('app.debug')) {
+            $data['message'] = $e->getMessage();
+            $data['file'] = $e->getFile();
+            $data['line'] = $e->getLine();
+            $data['context'] = $e->getTrace();
+        }
+
+        return response()->json($data, $data['code']);
     }
 }
