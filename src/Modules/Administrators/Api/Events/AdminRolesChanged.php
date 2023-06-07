0f10883ab11651147c6303c28d6ad33f8c5aaffe
daaeev <LfdblGbhju@gmail.com>
2023-06-07 18:31:03 +0300

    Added admin entity logic

---
+++ src/Modules/Administrators/Api/Events/AdminRolesChanged.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Project\Modules\Administrators\Api\Events;
+
+class AdminRolesChanged extends AbstractAdminEvent
+{
+
+}
--- src/Modules/Administrators/Entity/Admin.php
+++ src/Modules/Administrators/Entity/Admin.php
@@ -5,6 +5,9 @@ namespace Project\Modules\Administrators\Entity;
 use Project\Common\Events;
 use Webmozart\Assert\Assert;
 use Project\Common\Administrators\Role;
+use Project\Modules\Administrators\Api\Events\AdminLoginChanged;
+use Project\Modules\Administrators\Api\Events\AdminRolesChanged;
+use Project\Modules\Administrators\Api\Events\AdminPasswordChanged;
 
 class Admin implements Events\EventRoot
 {
@@ -24,6 +27,57 @@ class Admin implements Events\EventRoot
         Assert::notEmpty($name && $login && $password && $roles);
         Assert::allIsInstanceOf($roles, Role::class);
         $this->password = $password;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setPassword(string $password): void
+    {
+        if ($this->password === $password) {
+            return;
+        }
+
+        $this->password = $password;
+        $this->addEvent(new AdminPasswordChanged($this));
+    }
+
+    public function setLogin(string $login): void
+    {
+        if ($this->login === $login) {
+            return;
+        }
+
+        $this->login = $login;
+        $this->addEvent(new AdminLoginChanged($this));
+    }
+
+    public function setRoles(array $roles): void
+    {
+        Assert::allIsInstanceOf($roles, Role::class);
+        if ($this->sameRoles($roles)) {
+            return;
+        }
+
+        $this->roles = $roles;
+        $this->addEvent(new AdminRolesChanged($this));
+    }
+
+    private function sameRoles(array $roles): bool
+    {
+        if (count($roles) !== count($this->roles)) {
+            return false;
+        }
+
+        foreach ($roles as $role) {
+            if (!in_array($role, $this->roles)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function getId(): AdminId
